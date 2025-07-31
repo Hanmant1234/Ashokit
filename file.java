647f9278c4bae85e3a4d8a6eb283155c0b6dcd1b
Hanmant1234 <140038239+Hanmant1234@users.noreply.github.com>
2025-07-31 09:58:29 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class HelloHanmant {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Hanmant!");
     }
 }
